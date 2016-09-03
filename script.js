835a72a4e01032d6a53cbdcaef86b7266fe5f183
Dominic <spruxgaming@gmail.com>
2016-09-03 08:22:22 -0700

    new website focus

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-var button = document.getElementById('button');
-var audio = document.getElementById('audio');
+$(document).ready(function() {
+  $("#bannerarrow").on("click", function( e ) {
 
-var onClick = function() {
-    audio.play(); // audio will load and then play
-};
+    e.preventDefault();
 
-button.addEventListener('click', onClick, false);
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+
+  });
+});
+
+$(document).ready(function() {
+  $("#nav1").on("click", function( e ) {
+
+    e.preventDefault();
+
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+
+  });
+});
+
+$(document).ready(function() {
+  $("#nav2").on("click", function( e ) {
+
+    e.preventDefault();
+
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+
+  });
+});
