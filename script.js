d868e20ccf695b00261b1abf977f580a5582b259
Dominic <sprux@sprux.pro>
2016-10-20 18:42:56 -0700

    Fixed contact, better looking buttons

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+<<<<<<< HEAD
+/*
+=======
+
+>>>>>>> origin/master
+    for(var i = 0; i < 16; i++)
+    {
+      for(var j = audioChosen.currentTime; j == audioChosen.duration; j++)
+      {
+
+      if(j++ == audioChosen.duration){
+        nextAudio = randomAudio();
+        audioChosen = nextAudio;
+        nextAudio.play();
+        break;
+      }
+<<<<<<< HEAD
+
+      j = audioChosen.currentTime;
+      
+    }
+    }
+*/
+=======
+      j = audioChosen.currentTime;
+      
+    }
+
+    }
+>>>>>>> origin/master
+
